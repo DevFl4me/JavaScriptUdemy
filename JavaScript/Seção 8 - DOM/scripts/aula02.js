d59643f7015b63ea381a89d6cd2febf5e9b1cb38
Ryan Pablo <ryanpablobarbosa123@gmail.com>
2024-05-31 11:21:51 -0300

    Introdução DOM

---
+++ JavaScript/Seção 8 - DOM/scripts/aula02.js
@@ -0,0 +1,22 @@
+let a = document.querySelector('.link');
+
+// console.log(a.innerText);
+// console.log(a.parentNode);//body
+
+// console.log(a.parentNode.parentNode);//html
+
+// console.log(a.parentNode.parentNode.parentNode);//Document
+
+// console.log(a.parentNode.parentNode.parentNode.parentNode);//Null
+
+// console.log(a.parentNode.parentNode.childNodes)
+
+//  console.log(a.parentNode.parentNode.firstChild); // head
+
+//  console.log(a.firstChild);
+
+//  console.log(a.parentNode.parentNode.lastChild);//body
+
+// console.log(a.parentNode.parentNode.firstChild.nextSibling.firstChild.nextSibling);
+
+console.log(a.parentNode.parentNode.firstChild.nextSibling.previousSibling);
