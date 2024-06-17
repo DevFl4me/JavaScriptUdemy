52f9df1cb9f4425e4914a3fc02abaffb2a41c595
Ryan Pablo <ryanpablobarbosa123@gmail.com>
2024-06-17 10:53:11 -0300

    Async e Await

---
+++ JavaScript/Seção 11 - Js ES6/object shortSintaxe/index.js
@@ -0,0 +1,12 @@
+const nome = 'Programação em JavaScript';
+const preco = 'R$27.99';
+const horas = 24;
+
+ const cursos = {
+    nome,
+    preco,
+    instrutor : 'Geek University',
+    carga_horaria:horas,
+ };
+
+ console.log(cursos)
